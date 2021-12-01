f0de15eb7cc4c2331ad3142e92eaa48d92f5bd5b
IlanG96 <Ilangold123>
2021-12-01 13:16:44 +0200

    V 1.04

--- src/api/GraphAlgo.java
+++ src/api/GraphAlgo.java
@@ -1,7 +1,14 @@
 package api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
         DirectedWeightedGraph graph;
@@ -24,35 +31,63 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
                 return new Graph(graph);
         }
 
-
+        /**
+         * @return true if and only if (iff) there is a valid path from each node to each
+         * other node.
+         * NOTE: assume directional graph (all n*(n-1) ordered pairs).
+         * The function using a BFS algorithm that check for each node in the graph if he reached all the other nodes.
+         */
         @Override
         public boolean isConnected() {
-                Iterator<NodeData> Nconnect=graph.nodeIter();
-                Iterator<EdgeData> Econnect;
-                while(Nconnect.hasNext())
+                Iterator<NodeData> Nodes=graph.nodeIter();
+                while(Nodes.hasNext())
                 {
-                        int x=Nconnect.next().getKey();
-                        Econnect= graph.edgeIter(x);
-                   BFS(graph, x);
-                   while(Econnect.hasNext())
+                        int x=Nodes.next().getKey();
+                   BFS(x);
+                   Iterator<NodeData> Is_connected=graph.nodeIter();
+                   while(Is_connected.hasNext())
                    {
-                       if (Econnect.next().getTag()==-1) {
+                       if (Is_connected.next().getTag()==-1) {
                            return false;
                                 }
                         }
+                   RestTags();
                 }
                 return true;
         }
 
-        private void BFS(DirectedWeightedGraph graph, int v) {
-                while(graph.edgeIter(v).hasNext()){
-                        if(graph.edgeIter(v).next().getTag()==-1){
-                            int temp=graph.edgeIter(v).next().getDest();
-                                graph.getEdge(v,temp).setTag(1);
-                                DFS(graph,temp);
-                        }
-                        else continue;
+        /**
+         *         This function rest the tags of the nodes in the graph for the next BFS usage.
+         */
+        private void RestTags(){
+                Iterator<NodeData> Nodes=graph.nodeIter();
+                while (Nodes.hasNext()){
+                        Nodes.next().setTag(-1);
                 }
+        }
+
+        /**
+        This function is a BFS algorithm that check if the node sent to the function reach all the nodes in the graph.
+        if the node reach a certain node it change is tag to 1.
+        else the node tag will stay -1 which indicate the node hasn't been visited
+         */
+        private void BFS(int nodekey) {
+                Queue<NodeData>ConnectedTo= new LinkedList<>();
+                NodeData curr_node=this.graph.getNode(nodekey);
+                ConnectedTo.add(curr_node);
+                while (!ConnectedTo.isEmpty()){
+                        curr_node=ConnectedTo.poll(); //take the Node in the top of the queue
+                        if (curr_node.getTag()!=1) {  //check if you didnt vist him
+                                curr_node.setTag(1); //if you didnt visit him set his tag to 1 (1=visit him)
+                                Iterator<EdgeData> neighbors = this.graph.edgeIter(curr_node.getKey()); //go through his neighbors
+                                while (neighbors.hasNext()) {
+                                        NodeData temp_node=this.graph.getNode(neighbors.next().getDest());
+                                        ConnectedTo.add(temp_node); //add the neighbors to the queue.
+                                }
+                        }
+                }
+
+
         }
 
         @Override
@@ -82,6 +117,16 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
 
         @Override
         public boolean load(String file) {
-                return false;
+                Gson gson = new GsonBuilder()
+                        .registerTypeAdapter(DirectedWeightedGraph.class, new JsonToGraph())
+                        .create();
+                try {
+                        FileReader json = new FileReader(file);
+                        this.graph= gson.fromJson(json, DirectedWeightedGraph.class);
+                        return true;
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        return false;
+                }
         }
  }
